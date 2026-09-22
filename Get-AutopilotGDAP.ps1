eec12473fff03be1fd36b7a12a577f1422c6d5af
Michael van 't Hul <michaelvthul@hotmail.com>
2026-09-22 12:29:56 +0000

    Use legacy admin consent endpoint without scope requirement

--- Get-AutopilotGDAP.ps1
+++ Get-AutopilotGDAP.ps1
@@ -175,9 +175,9 @@ $LogonBtn.Add_Click({
             if ($res -eq 'Yes') {
                 # Een publiek client-id kan niet door dit script zelf als app-registratie
                 # worden aangemaakt. De tenant-admin moet de bestaande app eenmalig consent geven.
-                # Admin consent gebruikt een eigen endpoint; prompt=admin_consent op
-                # /oauth2/v2.0/authorize geeft AADSTS901001.
-                $consentUrl = "https://login.microsoftonline.com/organizations/v2.0/adminconsent?client_id=$Global:PublicClientId&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&redirect_uri=http%3A%2F%2Flocalhost"
+                # Gebruik het v1 admin-consent endpoint. Dit endpoint vereist geen
+                # scope-parameter en voorkomt AADSTS900144 in oudere tenants.
+                $consentUrl = "https://login.microsoftonline.com/organizations/adminconsent?client_id=$Global:PublicClientId&redirect_uri=http%3A%2F%2Flocalhost"
                 
                 try {
                     Set-Clipboard -Value $consentUrl -ErrorAction SilentlyContinue
@@ -276,7 +276,7 @@ $LoadProfilesBtn.Add_Click({
                     [System.Windows.MessageBox]::Show("Auto-injectie Mislukt. Geen rechten of de login is dichtgeklikt. Fout: $_", "Let op", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
                 }
 
-                $consentUrl = "https://login.microsoftonline.com/$Script:TargetTenantId/v2.0/adminconsent?client_id=$Global:PublicClientId&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&redirect_uri=http%3A%2F%2Flocalhost"
+                $consentUrl = "https://login.microsoftonline.com/$Script:TargetTenantId/adminconsent?client_id=$Global:PublicClientId&redirect_uri=http%3A%2F%2Flocalhost"
                 Set-Clipboard -Value $consentUrl
                 
                 try {
