a5f0733153b3c323525ff4ca6fc04a48695cc596
Michael van 't Hul <michaelvthul@hotmail.com>
2026-09-22 12:26:53 +0000

    Use valid Entra admin consent endpoint

--- Get-AutopilotGDAP.ps1
+++ Get-AutopilotGDAP.ps1
@@ -175,7 +175,9 @@ $LogonBtn.Add_Click({
             if ($res -eq 'Yes') {
                 # Een publiek client-id kan niet door dit script zelf als app-registratie
                 # worden aangemaakt. De tenant-admin moet de bestaande app eenmalig consent geven.
-                $consentUrl = "https://login.microsoftonline.com/organizations/adminconsent?client_id=$Global:PublicClientId"
+                # Admin consent gebruikt een eigen endpoint; prompt=admin_consent op
+                # /oauth2/v2.0/authorize geeft AADSTS901001.
+                $consentUrl = "https://login.microsoftonline.com/organizations/v2.0/adminconsent?client_id=$Global:PublicClientId&redirect_uri=http%3A%2F%2Flocalhost"
                 
                 try {
                     Set-Clipboard -Value $consentUrl -ErrorAction SilentlyContinue
@@ -274,7 +276,7 @@ $LoadProfilesBtn.Add_Click({
                     [System.Windows.MessageBox]::Show("Auto-injectie Mislukt. Geen rechten of de login is dichtgeklikt. Fout: $_", "Let op", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
                 }
 
-                $consentUrl = "https://login.microsoftonline.com/$Script:TargetTenantId/adminconsent?client_id=14d82eec-204b-4a57-966d-513373704195"
+                $consentUrl = "https://login.microsoftonline.com/$Script:TargetTenantId/v2.0/adminconsent?client_id=$Global:PublicClientId&redirect_uri=http%3A%2F%2Flocalhost"
                 Set-Clipboard -Value $consentUrl
                 
                 try {
